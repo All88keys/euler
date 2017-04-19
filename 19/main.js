03925d72f0dee4ce1fa001a2937b7f7a42734136
Christopher Vassallo <chrisvass1@gmail.com>
2017-04-19 10:14:03 -0400

    Create main.js

---
+++ 19/main.js
@@ -0,0 +1,19 @@
+var day = 1;
+
+var dateString = (day%31).toString()+"/"+(Math.floor(day/31)%12).toString()+"/"(1900+Math.floor(day/365)).toString
+console.log(dateString);
+
+
+var d = new Date("2/12/1900");
+var weekday = new Array(7);
+weekday[0] =  "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var n = weekday[d.getDay()];
+document.getElementById('day').innerHTML = n +" "+ d
+
